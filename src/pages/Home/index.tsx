fa78d35923252d9244c78a85205a108a05a9a6bb
Ansh Saxena <ansh.saxena@geminisolutions.com>
2024-06-15 19:14:06 +0530

    Initial commit...

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+      <div className="flex justify-center mt-2">
+        <p className="text-violet-600 text-4xl font-bold">Quizit Application</p>
+      </div>
+    );
+  };
+  
+  export default Home;
+  
